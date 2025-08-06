a619fdca7fa1717d0c36601a90c1750462e1d521
jaggyz <jaggyzmail@yahoo.com>
2025-08-06 16:45:36 +0700

    aa

--- atdd/automation-api/postman/entrypoint.sh
+++ atdd/automation-api/postman/entrypoint.sh
@@ -1,52 +1,19 @@
 # collection_files=$COLLECTION_FILES
-env_file_default=$ENV_DEFAULT_FILE
+env_file_default="./environments/default.postman_environment.json"
 count=1
 status_all=0
 
 # for file in $collection_files
 # Read file in postman folder
-for file in ./collections/*.postman_collection.json
+for file in ./*.postman_collection.json
 do
   tmp="$(basename -- $file)"
-  filename=${tmp//'.postman_collection.json'/''}
-  data_file="./data/$filename.postman_data.json"
-  env_file="./environments/$filename.postman_environment.json"
-
-  if [ -e $data_file ]; then
-    # ถ้ามี Data File จะใช้ ENV ตัวไหน
-    if [ -e $env_file ]; then
-      # ENV ใหม่
-      newman run $file -e $env_file -d $data_file --working-dir ./files \
-        -k -r htmlextra,cli,junit \
-        --env-var "HOST=$HOST" \
-        --reporter-htmlextra-export="reports/report_$count.html" \
-        --reporter-junit-export="reports/junitReport_$count.xml"
-    else
-      # ENV Default
-      newman run $file -e $env_file_default -d $data_file --working-dir ./files \
-        -k -r htmlextra,cli,junit \
-        --env-var "HOST=$HOST" \
-        --reporter-htmlextra-export="reports/report_$count.html" \
-        --reporter-junit-export="reports/junitReport_$count.xml"
-    fi
-  else
-    # ถ้าไม่มี Data File จะใช้ ENV ตัวไหน
-    if [ -e $env_file ]; then
-      # ENV ใหม่
-      newman run $file -e $env_file \
-        -k -r htmlextra,cli,junit \
-        --env-var "HOST=$HOST" \
-        --reporter-htmlextra-export="reports/report_$count.html" \
-        --reporter-junit-export="reports/junitReport_$count.xml"
-    else
-      # ENV Default
-      newman run $file -e $env_file_default --working-dir ./files \
-        -k -r htmlextra,cli,junit \
-        --env-var "HOST=$HOST" \
-        --reporter-htmlextra-export="reports/report_$count.html" \
-        --reporter-junit-export="reports/junitReport_$count.xml"
-    fi
-  fi
+  filename=${tmp/".postman_collection.json"/""}
+  collection_basename=${tmp%.postman_collection.json} # ลบนามสกุลไฟล์ ที่จะนำไปสร้าง Report HTMl
+    newman run $file \
+    -k -r htmlextra,cli,junit \
+    --reporter-htmlextra-export="reports/report_$collection_basename.html" \
+    --reporter-junit-export="reports/junitReport_$collection_basename.xml"
 
   status=$?
   if [ $status -eq 1 ]
